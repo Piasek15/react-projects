9330842e42fc70d481fdb1867ebe603288ad9c6a
Patrick Piasecki <piasecki.patrick.15@gmail.com>
2021-03-09 23:13:05 +0100

    project 1 and 2

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((tours) => {
+  //       setTours(tours);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
